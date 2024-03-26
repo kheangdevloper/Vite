1c21bb36e31aeac20c114f035521d5cc940630df
KHEANG <161300113+kheangdevloper@users.noreply.github.com>
2024-03-26 16:51:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Headers from './components/Headers'
+import Main from './components/Main'
+import Footers from './components/Footers'
+function App() {
+  return (
+    <div>
+    <Headers/>
+    <Main/>
+    <Footers/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
---
+++ src/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Headers from './components/Headers'
+import Main from './components/Main'
+import Footers from './components/Footers'
+function Home() {
+  return (
+    <div>
+      <Headers/>
+      <Main/>
+      <Footers/>  
+
+    </div>
+  )
+}
+
+export default Home
